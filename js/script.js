308a98baa5900ed1f523d7859c66a1681b6f05d4
wiliamtaesunglee <wiliam.tae@gmail.com>
2018-10-17 09:59:14 -0300

    correction of the location variables

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc, location2 = location1 + 1, location3 = location2 + 1;
 
 var guess;
 
@@ -8,16 +7,16 @@ var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
-while (isSunk == false); {
-  guesses = prompt("Redy, aim, fire!(enter a number 0-6):");
-  if (guess < 0 || guesses > 6 || null) {
+while (isSunk == false) {
+  guess = prompt("Redy, aim, fire!(enter a number 0-6):");
+  if (guess < 0 || guess > 6 || null) {
     alert("Please enter a valid cell number!");
-  }else {
-    guesses += 1;
-    if (guesses == location1 || guesses == location2 || guesses == location3) {
+  } else {
+    guesses = guesses + 1;
+    if (guess == location1 || guess == location2 || guess == location3) {
       alert("HIT!");
       hits += 1;
-      if (hits == 3) {
+      if (hits === 3) {
         isSunk = true;
         alert("You sank my battleship");
         } 
@@ -27,6 +26,6 @@ while (isSunk == false); {
   }
 }
 
-var status =   "you took " + guesses + "guesses to sink the batleship, " + "which means you shoopting accuaracy was " + (3/guesses);
+var status =   "you took " + guesses + " guesses to sink the batleship, " + "which means you shoopting accuaracy was " + (3/guesses);
 alert(status);
 
